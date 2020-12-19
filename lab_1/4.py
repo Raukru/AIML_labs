0adc6917c944471a662c2d7cff2a6d44a5896d29
Nikita Slivka <distian@inbox.ru>
2020-12-19 17:27:00 +0300

    fix'

--- lab_1/4.py
+++ lab_1/4.py
@@ -5,12 +5,11 @@ with open("sales.json", "r") as read_file:
 
 
 def json_to_csv(data):
-    csv_data = "item, country, year, sales\n"
+    csv_data = "item;country;year;sales\n"
     for items in data:
-        for keys in items.keys():
-            for country_key in items['sales_by_country']:
-                for year_key in items['sales_by_country'][country_key]:
-                    csv_data += items['item'] + ", " + country_key + ", " + str(year_key) + ", " + str(items['sales_by_country'][country_key][year_key]) + "\n"
+        for country_key in items['sales_by_country']:
+            for year_key in items['sales_by_country'][country_key]:
+                csv_data += items['item'] + ";" + country_key + ";" + str(year_key) + ";" + str(items['sales_by_country'][country_key][year_key]) + "\n"
     return csv_data
 
 
